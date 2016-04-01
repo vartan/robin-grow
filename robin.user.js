f45046f0f7bd6c2e96401fbb3a16ba047b878329
Michael Vartan <michael.vartan@student.csulb.edu>
2016-04-01 13:37:43 -0700

    incremented version

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Grow
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Try to take over the world!
 // @author       /u/mvartan
 // @include      https://www.reddit.com/robin*
