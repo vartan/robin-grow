16be5d9c9d8b3eda80a0be139e55a5c05cc4c76f
Michael Vartan <michael.vartan@student.csulb.edu>
2016-04-01 15:59:35 -0700

    kill old messages, actually

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Grow
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.41
 // @description  Try to take over the world!
 // @author       /u/mvartan
 // @include      https://www.reddit.com/robin*
@@ -104,6 +104,10 @@ function hashString(str) {
 var spamCounts = {};
 
 function findAndHideSpam() {
+    var messages = $(".robin--user-class--user");
+    for(var i = messages.length-1000; i >= 0; i--) {
+        $(messages[i]).remove()
+    }
     $('.robin-message--message:not(.addon--hide)').each(function() {
 
         // skips over ones that have been hidden during this run of the loop
