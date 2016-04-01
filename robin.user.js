838d5804fd55f9ddf6a3481c0dc4bbea57ed6b53
Michael Vartan <michael.vartan@student.csulb.edu>
2016-04-01 11:19:02 -0700

    now shows statistics

--- robin.user.js
+++ robin.user.js
@@ -7,11 +7,60 @@
 // @include      https://www.reddit.com/robin*
 // @grant        none
 // ==/UserScript==
+function addMins(date,mins) {
+    var newDateObj = new Date(date.getTime() + mins*60000);
+    return newDateObj;
+}
+
+function howLongLeft() { // mostly from /u/Yantrio
+    var remainingMessageContainer = $(".robin--user-class--system:contains('approx')");
+    var message = $(".robin-message--message", remainingMessageContainer).text();
+    var timestamp = $(".robin-message--timestamp",remainingMessageContainer).text();
+    var endTime;
+    try {
+        endTime = addMins(parseTime(timestamp),message.match(/\d+/)[0])
+    } catch(e){}
+    return Math.floor((endTime - new Date())/60/1000*10)/10;
+
+    //grab the timestamp from the first post and then calc the difference using the estimate it gives you on boot
+}
+function parseTime(timeStr, dt) {
+    if (!dt) {
+        dt = new Date();
+    }
+    var time;
+ try {
+    time = timeStr.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
+ } catch(e){}
+    if (!time) {
+        return NaN;
+    }
+    var hours = parseInt(time[1], 10);
+    if (hours == 12 && !time[3]) {
+        hours = 0;
+    }
+    else {
+        hours += (hours < 12 && time[3]) ? 12 : 0;
+    }
+ 
+    dt.setHours(hours);
+    dt.setMinutes(parseInt(time[2], 10) || 0);
+    dt.setSeconds(0, 0);
+    return dt;
+}
+
+
 
 (function() {
     'use strict';
+    $(".robin-chat--sidebar").prepend("<div class='addon' style='font-size:15pt'><div class='grows'></div><div class='stays'></div><div class='abandons'></div><div class='timeleft'></div></div>");
     var timeStarted = new Date();
-    function grow() {
+    function update() {
+        $(".timeleft").text(howLongLeft()+" minutes remaining");
+        $(".addon .grows").text("Grows: "+$(".robin-room-participant.robin--vote-class--increase").length);
+        $(".addon .abandons").text("Abandons: "+$(".robin-room-participant.robin--vote-class--abandon").length);
+        $(".addon .stays").text("Stays: "+$(".robin-room-participant.robin--vote-class--continue").length);
+
         var lastChatString = $(".robin-message--timestamp").last().attr("datetime");
         var timeSinceLastChat = new Date() - (new Date(lastChatString));
         var now = new Date();
@@ -22,12 +71,14 @@
             $(".text-counter-input").val("/vote grow").submit();
         }
     }
-grow();
+update();
+    
+
 setTimeout(function() {
             $(".text-counter-input").val("I automatically voted to grow, and so can you! http://redd.it/4cwk2s").submit();
 
 
 }, 10000);
-setInterval(grow, 1000);
+setInterval(update, 1000);
 
 })();
