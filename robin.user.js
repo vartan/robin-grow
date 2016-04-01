52f605bdb2f4ae0dd74bc45e8e84037178f84ae7
tslater2006 <tslater2006@gmail.com>
2016-04-01 16:53:24 -0500

    Updated version number

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Grow
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Try to take over the world!
 // @author       /u/mvartan
 // @include      https://www.reddit.com/robin*
