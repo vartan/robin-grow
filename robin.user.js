528d1c66538d4456e924c73ef58d18af98351a50
Matan Kushner <matchai@me.com>
2016-04-02 02:29:05 -0400

    Increment script version

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Grow
 // @namespace    http://tampermonkey.net/
-// @version      1.59
+// @version      1.60
 // @description  Try to take over the world!
 // @author       /u/mvartan
 // @include      https://www.reddit.com/robin*
