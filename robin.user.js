b9495d68f58a463a8b4c46221eb522c2aa969b6f
5a1t <salt@luci.fr>
2016-04-03 20:52:41 -0700

    increase version

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Grow (modified multichat)
 // @namespace    http://tampermonkey.net/
-// @version      1.92
+// @version      1.95
 // @description  Try to take over the world!
 // @author       /u/mvartan
 // @include      https://www.reddit.com/robin*
