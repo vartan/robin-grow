334987ee8c2752c96dfc7b8c810680f8ca8b0b00
5a1t <salt@luci.fr>
2016-04-05 01:06:27 -0700

    version bump

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         parrot (color multichat for robin!)
 // @namespace    http://tampermonkey.net/
-// @version      2.43
+// @version      2.45
 // @description  Recreate Slack on top of an 8 day Reddit project.
 // @author       dashed, voltaek, daegalus, vvvv, orangeredstilton
 // @include      https://www.reddit.com/robin*
