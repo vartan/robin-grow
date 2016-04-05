9b83c4fb7d4a6a86d58b9d7a0eb62111730991d1
Alberto <mail4alberto@gmail.com>
2016-04-05 02:06:20 -0400

    v2.37 fml

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         parrot (color multichat for robin!)
 // @namespace    http://tampermonkey.net/
-// @version      2.36
+// @version      2.37
 // @description  Recreate Slack on top of an 8 day Reddit project.
 // @author       /u/_vvvv_
 // @include      https://www.reddit.com/robin*
