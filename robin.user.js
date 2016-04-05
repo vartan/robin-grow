bff060034eaa3cfa1c79c1b7a9073c2884d685ea
5a1t <salt@luci.fr>
2016-04-04 19:48:03 -0700

    v bump for scrolly

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         parrot (color multichat for robin!)
 // @namespace    http://tampermonkey.net/
-// @version      2.26
+// @version      2.28
 // @description  Try to take over the world!
 // @author       /u/_vvvv_
 // @include      https://www.reddit.com/robin*
